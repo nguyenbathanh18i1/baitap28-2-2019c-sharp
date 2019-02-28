fde863a1d82800d4e9a2601106e9e0735f9e522d
nguyenbathanh18i1 <48078094+nguyenbathanh18i1@users.noreply.github.com>
2019-02-28 23:26:40 +0700

    Add files via upload

---
+++ baitap1d.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baitap1d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n4, s4 = 0;
+            Console.Write("nhap n4:\n");
+            n4 = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n4; i++)
+            {
+                if (n4 % i == 0)
+                    s4 += i;
+            }
+            Console.WriteLine("Ket qua la: {0}", s4);
+            Console.ReadLine();
+        }
+    }
+}
